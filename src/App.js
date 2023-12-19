57a449bbc6223c6075fea75dc322d1bfb6b7951f
sneh-soni <ssneh20062003@gmail.com>
2023-12-19 22:18:59 +0530

    2_header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "./components/Login";
 import Browse from "./components/Browse";
+import Home from "./components/Home";
 
 function App() {
   const appRouter = createBrowserRouter([
     {
       path: "/",
-      element: <Login />,
+      element: <Home />,
     },
     {
       path: "/browse",
       element: <Browse />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
     },
   ]);
   return (
